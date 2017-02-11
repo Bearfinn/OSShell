c9004d479a22fcdd8dbd20936540074a30ddc959
coffeelover1404 <nansineelove@hotmail.com>
2017-02-11 12:34:54 +0700

    comment code

--- shell.c
+++ shell.c
@@ -7,28 +7,30 @@
 
 int main( int argc, char *argv[], char *env[] )
 {
-	pid_t child_pid;
+    //declare value
+    pid_t child_pid;
    int status;
-   char command[1001];
-   char history[1001][1001];
+   char command[1001]; //to keep the input command
+   char history[1001][1001]; //history list
    char *token;
-   char *args[10];
+   char *args[10]; //keep argument
 
 	int i = 0;
 	int commandCount = 0;
 	while (1) {
 		command[0] = '\0';
-		printf("[CustomShell] ");
-		fgets(command, 1000, stdin);
+		printf("[CustomShell] "); //telling user that the program ready for getting input
+		fgets(command, 1000, stdin); //get the input
 		command[strlen(command) - 1] = '\0';
 
-		if (strcmp(command, "history") == 0) {
+		if (strcmp(command, "history") == 0) //checking that command is history or not
+        {
 			int j = 0;
 			for (j = 0; j < commandCount; j++)
 			{
-				printf("%d. %s\n", j+1, history[j]);
+				printf("%d. %s\n", j+1, history[j]); //print out the history
 			}
-			strcpy(history[commandCount], command);
+			strcpy(history[commandCount], command); //then add the history command into history list
 			commandCount++;
 			continue;
 		} else if (strcmp(command, "exit") == 0) {
@@ -37,21 +39,21 @@ int main( int argc, char *argv[], char *env[] )
 			strcpy(history[commandCount], command);
 			commandCount++;
 		}
-		
+
 		//printf("Untokenized: %s\n", command);
-		token = strtok(command, " ");
-		while( token != NULL ) 
+		token = strtok(command, " "); //separate white space
+		while( token != NULL )
 		{
 			args[i] = token;
 			i++;
 		   token = strtok(NULL, " ");
 		}
-		if((child_pid = fork()) < 0)
+		if((child_pid = fork()) < 0) //if the fork fail
 		{
 		   perror("Fork Failed: ");
 		   exit(1);
 		}
-		else if(child_pid == 0)
+		else if(child_pid == 0) //
 		{
 		   execvp(command, args);
 		   perror("Execution Failed: ");
@@ -59,7 +61,7 @@ int main( int argc, char *argv[], char *env[] )
 		}
 		else
 		{
-		   wait(NULL);
+		   wait(NULL); //waiting other fork to running
 		}
 	}
    return 0;
